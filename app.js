3b6c1947475f1b698822f8f7c79346ecbc78eb84
Louis Gadza <siuolgadza@gmail.com>
2022-10-10 17:36:34 +0200

    u

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 document.getElementById("button").onclick = function () {
   document.getElementById("shopname").innerText = "Impuls Gaming Parlor";
+  document.getElementById("shopname").style.color = "rgba(255, 128, 82, 0.6)";
 };
 
 document.getElementById("button2").onclick = function () {
   document.querySelector("html").style.backgroundColor =
     "rgba(213, 189, 181, 0.6)";
 };
-const fakeAddress = function () {
-  document.getElementById("address").innerHTML = `<div>
-          <address>15135 Pumula South,</address>
-          <address>Bulawayo,</address>
-          <address>Zimbabwe, 15-333</address>
-        </div>`;
-};
+
 document.getElementById("change").onclick = function () {
   document.getElementById("address").innerHTML = `<div>
           <address>15135 Pumula South,</address>
@@ -21,7 +16,6 @@ document.getElementById("change").onclick = function () {
         </div>`;
 };
 
-// fakeAddress();
 const addClass = function () {
   for (i = 0; i < a.length; i++) {
     const elem = document.querySelectorAll("a");
